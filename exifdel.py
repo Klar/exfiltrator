d3c5e673080dabb0f34e9c6fe6a5654fe2102e21
klar <arvet.fuchs@gmail.com>
2017-01-19 22:22:11 +0100

    initial version of exfiltrator.py

---
+++ exifdel.py
@@ -0,0 +1,21 @@
+import os, sys
+#pip install Pillow
+from PIL import Image as Image
+import uuid
+
+
+file_path = "removeexif"
+
+for imagename in os.listdir(file_path):
+
+    image = Image.open(file_path + "/" + imagename)
+    data = list(image.getdata())
+    image_without_exif = Image.new(image.mode, image.size)
+    image_without_exif.putdata(data)
+
+    image_without_exif.save("stripped" + "/" + uuid.uuid4().hex + ".jpg")
+    os.remove(file_path + "/" + imagename)
+
+    print("[*] File Saved: Exif_Stripped_%s" % (imagename))
+
+sys.exit(0)
